3c05648b44e12a642b0ac6da397a015a4b38670e
Anni3607 <anirudhapujari7@gmail.com>
2026-05-26 20:37:59 +0530

    Add files via upload

---
+++ modules/domain_criteria.py
@@ -0,0 +1,59 @@
+DOMAIN_CRITERIA = {
+
+    "Career": [
+
+        "salary",
+        "career_growth",
+        "work_life_balance",
+        "flexibility",
+        "stress",
+        "learning"
+    ],
+
+    "Education": [
+
+        "fees",
+        "placements",
+        "brand_value",
+        "difficulty",
+        "roi",
+        "global_opportunities"
+    ],
+
+    "Investment": [
+
+        "returns",
+        "risk",
+        "liquidity",
+        "stability",
+        "growth_potential"
+    ],
+
+    "Tech Purchase": [
+
+        "performance",
+        "battery",
+        "camera",
+        "price",
+        "longevity",
+        "design"
+    ],
+
+    "Food": [
+
+        "taste",
+        "nutrition",
+        "price",
+        "availability",
+        "healthiness"
+    ],
+
+    "Fitness": [
+
+        "effectiveness",
+        "difficulty",
+        "time_required",
+        "sustainability",
+        "cost"
+    ]
+}
