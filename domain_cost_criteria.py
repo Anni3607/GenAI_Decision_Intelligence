d99aee1a397d7bcff9d355947728d192292ff166
Anni3607 <anirudhapujari7@gmail.com>
2026-05-28 15:04:53 +0530

    Add files via upload

---
+++ domain_cost_criteria.py
@@ -0,0 +1,33 @@
+DOMAIN_COST_CRITERIA = {
+
+    "Career": [
+        "stress"
+    ],
+
+    "Education": [
+        "fees",
+        "difficulty"
+    ],
+
+    "Investment": [
+        "risk"
+    ],
+
+    "Tech Purchase": [
+        "price"
+    ],
+
+    "Food": [
+        "price"
+    ],
+
+    "Fitness": [
+        "difficulty",
+        "time_required",
+        "cost"
+    ],
+
+    "Fashion": [
+        "price"
+    ]
+}
